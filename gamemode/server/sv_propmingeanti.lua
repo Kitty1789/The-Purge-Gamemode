6d4cc1c4efe2ff7d72de0175426ff1b38aff0d29
Lisa <WhiteKitty00@users.noreply.github.com>
2017-03-02 08:15:59 +0100

    Update sv_propmingeanti.lua

--- gamemode/server/sv_propmingeanti.lua
+++ gamemode/server/sv_propmingeanti.lua
@@ -1,14 +1,14 @@
 hook.Add('PlayerInitialSpawn', 'Anti-Proptouch', function(ply)
 	ply:SetTrigger(true)
 	function ply:Touch(ent)
-		if IsValid(ent) and ent:GetClass() == 'prop_*' and ent:IsFrozen() != true then
+		if IsValid(ent) and ent:GetClass() == 'prop_physics' and ent:IsFrozen() != true then
 			ent:Freeze(true)
 		end
 	end
 end)
 
 hook.Add('PlayerShouldTakeDamage', 'Anti-Propdamage', function(ply, ent)
-	if IsValid(ent) and ent:GetClass() == 'prop_*' then
+	if IsValid(ent) and ent:GetClass() == 'prop_physics' then
 		return false
 	end
 end)
